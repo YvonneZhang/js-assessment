f3b9f9ec6b4324858ed1a283016909820a36741a
yvonnezhang <zhyifa001@gmail.com>
2017-12-16 21:22:33 +0800

    feat: string tests all passed

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,52 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    let wordCount = 0,
+      ch = '';
+    return str.split('').reduce((accumulation, currentCh) => {
+      if (ch !== currentCh) {
+        // 清空字符计数器
+        ch = currentCh;
+        wordCount = 1;
+        return accumulation + ch;
+      }
+      wordCount++;
+      if (wordCount === amount) {
+        return accumulation + ch.repeat(wordCount - 1);
+      }
+      return accumulation;
+    }, '');
   },
 
   wordWrap: function(str, cols) {
-
+    const words = str.split(' ');
+    let substr = '';
+    return words.reduce((acc, curWord, curIndex, past) => {
+      if (curWord.length >= cols) {
+        return acc + curWord + '\n';
+      }
+      const nextWord = words[curIndex + 1];
+      if (!nextWord) {
+        return acc + curWord;
+      }
+      if (curWord.length + nextWord.length > cols) {
+        return acc + curWord + '\n';
+      }
+      if (curWord.length + nextWord.length < cols) {
+        const lastIdx = acc.lastIndexOf('/n');
+        if (acc.length - lastIdx >= cols) {
+          return acc + curWord + '\n';
+        }
+        return acc + curWord + ' ';
+      }
+      return acc;
+    }, '');
   },
 
   reverseString: function(str) {
-
+    return str
+      .split('')
+      .reverse()
+      .join('');
   }
 };
