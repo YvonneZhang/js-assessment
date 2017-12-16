cb519019bbe80096beeecdb5593b6073246dff16
yvonnezhang <zhyifa001@gmail.com>
2017-12-16 22:09:44 +0800

    feat: async tests all pass.

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise(resolve => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(this.response);
+      };
+      xhr.open('GET', url);
+      xhr.send();
+    }).then(response => {
+      const data = JSON.parse(response);
+      return data.people.map(value => value.name).sort((a, b) => {
+        return a > b;
+      });
+    });
   }
 };
